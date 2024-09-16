a0b749d0e8fd73114c28a6e3f3f5167cf154d223
Elmer Augusto Riva Rodriguez <67970179+elmer-riva@users.noreply.github.com>
2024-09-15 22:06:58 -0500

    fix: update confi.py with valid user and password.

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:sugurugeto@10.0.0.4/FloraYFauna'
+    SQLALCHEMY_DATABASE_URI = 'mysql://augusto:augustoriva@10.0.0.4/FloraYFauna'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
